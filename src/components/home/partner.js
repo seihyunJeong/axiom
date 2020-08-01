c15fc622b4c7bb95ae9fc1112c7cdffcf078756d
Seihyun <37438619+seihyunJeong@users.noreply.github.com>
2020-08-01 15:33:18 +0900

    hover test 2

--- src/components/home/partner.js
+++ src/components/home/partner.js
@@ -100,6 +100,7 @@ const Partner = () => {
                     ({node.category})
                   </Name>
                 <Logo src={`/partner/${node.id}.png`} onMouseOver={`src=/partner/${node.id}_hover.png`} onMouseOut={`src=/partner/${node.id}.png`}/>   
+                <Logo src={`/partner/${node.id}.png`} onMouseOver={() => { src=`/partner/${node.id}_hover.png`; console.log('mouse over'); }} onMouseOut={() => { src=`/partner/${node.id}.png`; console.log("mouse out");}}/>
 */
   return (
     <Container>
@@ -112,7 +113,7 @@ const Partner = () => {
             <Item>
               <a key={node.id} href={node.link} target="_blank" rel="noopener noreferrer">
                 <ItemContent>
-                <Logo src={`/partner/${node.id}.png`} onMouseOver={() => { src=`/partner/${node.id}_hover.png`; console.log('mouse over'); }} onMouseOut={() => { src=`/partner/${node.id}.png`; console.log("mouse out");}}/>
+                <Logo src={`/partner/${node.id}.png`} onMouseOver={() => { console.log(this); console.log('mouse over'); }} onMouseOut={() => { src=`/partner/${node.id}.png`; console.log("mouse out");}}/>
                 
                 </ItemContent>
               </a>
